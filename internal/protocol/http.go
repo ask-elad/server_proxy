1d0ef2710ba10b9fd154bd3416ac61b123696a3f
Aryan Rai <aryanrai746@gmail.com>
2025-12-23 05:09:56 +0530

    added http handling

--- internal/protocol/http.go
+++ internal/protocol/http.go
@@ -1,11 +1,88 @@
 package protocol
 
-import "net"
+import (
+	"io"
+	"net"
+	"net/url"
+	"strings"
+)
+
+type HTTPRequest struct {
+	Method  string
+	Version string
+	Target  string
+	Path    string
+}
 
 func HandleHTTP(client net.Conn, res *Result) {
-	// TODO:
-	// - parse absolute-form request line
-	// - extract host:port
-	// - rewrite request line
-	// - forward request + response
+
+	fields := strings.Fields(res.FirstLine)
+	if len(fields) != 3 {
+		return
+	}
+
+	method := fields[0]
+	rawURL := fields[1]
+	version := fields[2]
+
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return
+	}
+
+	if parsedURL.Scheme != "http" {
+		return
+	}
+
+	host := parsedURL.Hostname()
+	if host == "" {
+		return
+	}
+
+	port := parsedURL.Port()
+	if port == "" {
+		port = "80"
+	}
+
+	path := parsedURL.RequestURI()
+	if path == "" {
+		path = "/"
+	}
+
+	targetAddr := host + ":" + port
+
+	req := HTTPRequest{
+		Method:  method,
+		Version: version,
+		Target:  targetAddr,
+		Path:    path,
+	}
+
+	targetConn, err := net.Dial("tcp", req.Target)
+	if err != nil {
+		return
+	}
+
+	clientTCP, _ := client.(*net.TCPConn)
+	targetTCP, _ := targetConn.(*net.TCPConn)
+
+	rewrittenFirstLine := req.Method + " " + req.Path + " " + req.Version + "\r\n"
+	_, err = targetConn.Write([]byte(rewrittenFirstLine))
+	if err != nil {
+		targetConn.Close()
+		return
+	}
+
+	go func() {
+		_, _ = io.Copy(targetConn, res.Reader)
+		if targetTCP != nil {
+			targetTCP.CloseWrite()
+		}
+	}()
+
+	_, _ = io.Copy(client, targetConn)
+
+	if clientTCP != nil {
+		clientTCP.CloseWrite()
+	}
 }
