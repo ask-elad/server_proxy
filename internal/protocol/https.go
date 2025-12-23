3e21e11fb80253800ff73cd414534f48827139c3
Aryan Rai <aryanrai746@gmail.com>
2025-12-23 05:31:16 +0530

    added https handling

--- internal/protocol/https.go
+++ internal/protocol/https.go
@@ -1,11 +1,46 @@
 package protocol
 
-import "net"
+import (
+	"io"
+	"net"
+	"strings"
+)
 
 func HandleCONNECT(client net.Conn, res *Result) {
-	// TODO:
-	// - parse host:port from CONNECT line
-	// - dial target
-	// - send "200 Connection Established"
-	// - switch to raw TCP forwarding
+
+	fields := strings.Fields(res.FirstLine)
+	if len(fields) != 3 {
+		return
+	}
+
+	targetAddr := fields[1]
+	if !strings.Contains(targetAddr, ":") {
+		return
+	}
+
+	targetConn, err := net.Dial("tcp", targetAddr)
+	if err != nil {
+		return
+	}
+
+	clientTCP, _ := client.(*net.TCPConn)
+	targetTCP, _ := targetConn.(*net.TCPConn)
+
+	_, err = client.Write([]byte("HTTP/1.1 200 Connection Established\r\n\r\n"))
+	if err != nil {
+		targetConn.Close()
+		return
+	}
+
+	go func() {
+		_, _ = io.Copy(targetConn, res.Reader)
+		if targetTCP != nil {
+			targetTCP.CloseWrite()
+		}
+	}()
+
+	_, _ = io.Copy(client, targetConn)
+	if clientTCP != nil {
+		clientTCP.CloseWrite()
+	}
 }
